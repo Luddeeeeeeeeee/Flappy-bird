2a570b09b59b89a91d78f5fcf355fdec471a6b39
Ludwig Warstedt <ludwig.warstedt@outlook.com>
2023-09-01 15:54:52 +0200

    tfygui

--- flappy bird.py
+++ flappy bird.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 from pygame import *
 import time
 pygame.init
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode((500,500))
 
 
 playerd = pygame.image.load("player.png")
+block_1 = pygame.image.load("Block.png")
+block_1_x = 400
+
 playerd_x = 230
 playerd_y = 255
 gravity = 0
@@ -16,6 +19,7 @@ while run:
     class player:
         def drawing():
             global playerd
+            
             screen.blit(playerd,(playerd_x,playerd_y))
         def gravity():
             global playerd_y,gravity
@@ -26,13 +30,19 @@ while run:
 
 
             
-            
+    class block_obstical:
+        def drawing():
+            block_1_y = 0
+            screen.blit(block_1,(block_1_x, block_1_y))
+        def moving():
+            global block_1_x
+            block_1_x -= 1
 
 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key ==  pygame.K_SPACE:
-                gravity -= 10
+                gravity -= 20
         if event.type == pygame.QUIT:
             run = False
 
@@ -42,5 +52,8 @@ while run:
     Player = player
     Player.drawing()
     Player.gravity()
+    Block = block_obstical
+    Block.drawing()
+    Block.moving()
     pygame.display.flip()
     Clock.tick(60)
